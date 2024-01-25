3989fc5b02d86387051fe2fd0a729029c94419ab
ColinAdo <colinadodev30@gmail.com>
2024-01-25 08:35:50 +0300

    Add tests to Todo Model

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+class TodoTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title = 'Test Title',
+            body = 'Test body',
+        )
+
+    def test_todo_content(self):
+        self.assertEqual(self.todo.title, 'Test Title')
+        self.assertEqual(self.todo.body, 'Test body')
+        self.assertEqual(self.todo.__str__(), 'Test Title')
+
